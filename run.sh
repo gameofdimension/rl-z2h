9181ad4ecf324b8a6c82566a756b4d7972fc4df5
G.O.D <32255912+gameofdimension@users.noreply.github.com>
2025-02-22 08:27:37 +0000

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-python dqn.py  --env-id CartPole-v1 --track --wandb-project-name cleanrl-cartpole --capture_video
+set -euxo pipefail
+
+python dqn.py  --env-id CartPole-v1 --track --wandb-project-name cleanrl-cartpole --capture_video $@
